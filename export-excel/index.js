c1ce24c14c5ba09e5bc12bb31cf31db1bf71bc72
Administrator <1787009521@qq.com>
2020-10-21 11:49:00 +0800

    数据导出execl

---
+++ export-excel/index.js
@@ -0,0 +1,27 @@
+const ExportJsonExcel = require("js-export-excel");
+
+var option = {};
+
+option.fileName = "excel";
+
+option.datas = [
+    {
+        sheetData: [
+            { one: "一行一列", two: "一行二列" },
+            { one: "二行一列", two: "二行二列" },
+        ],
+        sheetName: "sheet",
+        sheetFilter: ["two", "one"],
+        sheetHeader: ["第一列", "第二列"],
+        columnWidths: [20, 20],
+    },
+    {
+        sheetData: [
+            { one: "一行一列", two: "一行二列" },
+            { one: "二行一列", two: "二行二列" },
+        ],
+    },
+];
+
+var toExcel = new ExportJsonExcel(option); //new
+toExcel.saveExcel(); //保存
